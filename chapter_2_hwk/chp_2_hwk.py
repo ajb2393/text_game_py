47fa8a752bdc2ac4fb5e6c6cacc5fbb01101996d
ajb2393 <ajb23293@gmail.com>
2023-03-06 17:40:24 +0000

    Updated

--- chapter_2_hwk/chp_2_hwk.py
+++ chapter_2_hwk/chp_2_hwk.py
@@ -1,1 +1,17 @@
+#This file will address all homework questions for chapter 2
 
+def calculator_sum(input_x, input_y):
+    sum_of_inputs = input_x + input_y 
+    return sum_of_inputs 
+
+print(f"The sum of {23} and {6} is: ")
+
+sum_total = calculator_sum(23, 6)
+
+print(f"{sum_total}")
+
+
+user_name = input("What is your name?\n")
+print(f"Your name is {user_name}!")
+
+    
