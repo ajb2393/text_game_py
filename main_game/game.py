eeb5790449e587b791c99243906c797725df6447
ajb2393 <ajb23293@gmail.com>
2023-03-08 19:17:26 +0000

    Changes and Updates

--- main_game/game.py
+++ main_game/game.py
@@ -1,7 +1,17 @@
 #Welcome to Greed Island
 
 print("Escape from Greed Island -_-")
+action_input = input("Where would you like to go?:\n")
 
-action_input = input("Action:\n")
-print(f"We're now moving {action_input}")
-print("Keep on going")
+if action_input == 'n':
+    print("You're moving North")
+elif action_input == 'e':
+    print("You're moving East")
+elif action_input == 's':
+    print("You're moving South")
+elif action_input == 'w':
+    print("You're moving West") 
+else: 
+    print("The submitted direction is wrong; try again")
+
+
