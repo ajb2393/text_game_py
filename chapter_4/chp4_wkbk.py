a719dfd9ca8ee0b149b9843308766e5abae7cf58
ajb2393 <ajb23293@gmail.com>
2023-03-08 19:12:49 +0000

    Genesis

--- chapter_4/chp4_wkbk.py
+++ chapter_4/chp4_wkbk.py
@@ -1,1 +1,21 @@
+#Main workbook exercises for chapter 4 
+
+print("Welcome to Heart Break") 
+
+n = 50 
+if n < 100:
+    print("This condition is true!")
+else: # <-- This part is optional
+    print("The condition is false!")
+
+m = 300 
+
+if m < 100: 
+    print("m is less than 100")
+elif m < 250: 
+    print("m is less than 250")
+else: 
+    print("Half a ticket all in")
+
+
 
